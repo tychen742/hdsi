789c5ac05fa98ef718adf085c2d11c375ca3705c
Tsangyao Chen <tychen742@gmail.com>
2024-12-29 14:43:33 -0600

    committed @ 2024-1229-144333

--- includes/topnav.php
+++ includes/topnav.php
@@ -418,3 +418,6 @@ WHERE id_user = '$uid_hbdi' ")->fetchAll();
 unset($id_project);
 include_once("/var/www/hdsi/scripts/modals.php");
 ?>
+
+<!-- ##### bootstrap JS bundle-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
