a6f95ce31237b4f4ba1429678112099a97d2d5ff
Tsangyao Chen <tychen742@gmail.com>
2024-12-29 15:21:51 -0600

    committed @ 2024-1229-152151

--- includes/topnav.php
+++ includes/topnav.php
@@ -41,25 +41,6 @@ if (!isset($_SESSION['email_hbdi'])) {
     // echo "TTTTkkTTTTTT";
     ?>
 
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-
-    <body>
-    <!--[if lt IE 7]><p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade
-                                                                                                                   your
-                                                                                                                   browser</a>
-                                            to improve your experience.</p><![endif]-->
-
-    <script src="" async defer></script>
-    </body>
-
-    </html>
     <!--        ##### topnav wrapper #####-->
     <div class="topnav_wrapper">
         <!-- ##### the Navigation Bar navbar ##### -->
