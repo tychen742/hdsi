7aa72e00b51424c0c6144cd9b7d2b6b72a90650d
Tsangyao Chen <tychen742@gmail.com>
2024-12-29 02:19:36 -0600

    committed @ 2024-1229-021936

--- includes/headers.php
+++ includes/headers.php
@@ -3,7 +3,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 include_once('headers.php');
-include_once('db_conn.php');
+// include_once('db_conn.php');
 //include_once('login_loader.php');
 $server_name = $_SERVER['SERVER_NAME'];
 if ($server_name == 'hdsinet.us') {
