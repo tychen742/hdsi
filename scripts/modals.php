9f9cfad39a0af3f0b95ca8c81edc5cd1a9692aa1
Tsangyao Chen <tychen742@gmail.com>
2024-12-29 23:34:55 -0600

    committed @ 2024-1229-233455

--- scripts/modals.php
+++ scripts/modals.php
@@ -149,7 +149,7 @@ include_once("/var/www/hdsi/includes/utilities.php");
 <!-- TODOO: solved. it's requiring double log-in to log into the system. Why??? -->
 <div class="modal fade" id="loginModal" role="dialog" tabindex="-1" aria-labelledby="loginModal" aria-hidden="true">
     <div class="modal-dialog">
-        <!-- ##### Modal content-->
+        <div class="modal-content">
             <div class="modal-header">
                 <div class="modal-header">
                     <div class="modal-title"> Log In</div>
@@ -413,7 +413,8 @@ include_once("/var/www/hdsi/includes/utilities.php");
 <!-- ##### script for SignUp Modal form checks ##### -->
 <div class="modal fade" id="signupModal" role="dialog" tabindex="-1" aria-labelledby="loginModal" aria-hidden="true">
     <div class="modal-dialog">
-        <!-- ##### Modal content-->
+
+        <!-- Modal content-->
         <div class="modal-content">
             <form enctype="multipart/form-data" method="POST" onsubmit="return validate();">
                 <div class="modal-header">
