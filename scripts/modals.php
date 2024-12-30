fc7bc66e2a2208f9308e6d046546854479460be4
Tsangyao Chen <tychen742@gmail.com>
2024-12-30 15:10:46 -0600

    committed @ 2024-1230-151046

--- scripts/modals.php
+++ scripts/modals.php
@@ -152,16 +152,16 @@ include_once("/var/www/hdsi/includes/utilities.php");
 
         <div class="modal-content">
 
-            <form method="POST" action="">
-                <div class="modal-header">
-                    <div class="modal-title"> Log In</div>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true"></span>
-                    </button>
-                </div>
+            <div class="modal-header">
+                <div class="modal-title"> Log In</div>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true"></span>
+                </button>
+            </div>
 
-                <div class="modal-body">
+            <div class="modal-body">
 
+                <form method="POST">
                     <div class="mb-3">
                         <!--                        <label>Email address</label>-->
                         <label for="recipient-email" class="col-form-label"></label> <input type="text"
@@ -179,30 +179,32 @@ include_once("/var/www/hdsi/includes/utilities.php");
                             placeholder="password">
                         <!--                            <input type="password" placeholder="password" name="password" class="form-control">-->
                     </div>
-                </div>
+                </form>
+            </div>
 
-                <div class="modal-footer" style="display: block; text-align: right">
-                    <!-- <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button> -->
-                    <button class="btn" data-bs-toggle="modal" type="button" data-dismiss="modal"
-                        data-bs-target="#loginModal" name="submitLogIn">
-                        Log In
-                    </button>
+            <div class="modal-footer" style="display: block; text-align: right">
+                <!-- <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button> -->
 
-                    <br>
 
-                    <button class="btn" data-bs-toggle="modal" type="button" data-dismiss="modal"
-                        data-bs-target="#signupModal" name="submitSignUp"
-                        style="width: auto; background-color: #915664" value="">
-                        Sign Up
-                    </button>
+                <button class="btn" data-bs-toggle="modal" type="button" data-dismiss="modal"
+                    data-bs-target="#loginModal" name="submitLogIn">
+                    Log In
+                </button>
 
-                    <button class="btn" data-bs-toggle="modal" type="button" data-dismiss="modal"
-                        data-bs-target="#resetPwModal" name="resetPwModal"
-                        style="width: auto; background-color: #915664" value="">
-                        Reset Password
-                    </button>
-                </div>
-            </form>
+                <br>
+
+                <button class="btn" data-bs-toggle="modal" type="button" data-dismiss="modal"
+                    data-bs-target="#signupModal" name="submitSignUp"
+                    style="width: auto; background-color: #915664" value="">
+                    Sign Up
+                </button>
+
+                <button class="btn" data-bs-toggle="modal" type="button" data-dismiss="modal"
+                    data-bs-target="#resetPwModal" name="resetPwModal"
+                    style="width: auto; background-color: #915664" value="">
+                    Reset Password
+                </button>
+            </div>
             <!-- <div>
                     Forget your password? <span style="color: #915664; font-weight: 500" data-toggle="modal"
                         data-target="#resetPwModal"
@@ -253,10 +255,7 @@ if (isset($_POST['submitLogIn'])) {
         // ##### check Account Activation #####
         if ($user_time_verified) {
             // ##### verify password #####
-            // $isValid = password_verify($password_posted, $password_from_db);
-            if ($password_posted == $password_from_db) {}
-            $isValid = true;
-        }
+            $isValid = password_verify($password_posted, $password_from_db);
             if ($isValid) {
                 // ##### Create SESSIONS ##### //{
                 error_log("Password is a match", 0);
@@ -331,7 +330,7 @@ if (isset($_POST['submitLogIn'])) {
         echo "TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT";
         exit;
     }
-
+}
 ?>
 </div>
 <!-- ##### end of log in login processing ##### -->
@@ -506,17 +505,17 @@ if (isset($_POST['submitLogIn'])) {
                     value="sign up"> Sign Up
                 </button>
                 <br>
-                <button class="btn" style="float: right; background-color: #915664"
+                <button class="btn" style="float: right; background-color: #915664" 
                     data-toggle="modal"
                     data-target="#loginModal"
                     data-dismiss="modal"> log in
                 </button>
 
-                <!-- <button class="btn" style="float: right; background-color: #915664"  -->
-                <button class="btn" style="float: right; background-color: #000000"
+                <button class="btn" style="float: right; background-color: #915664" 
                     data-toggle="modal"
                     data-target="#resetPwModal"
-                    data-dismiss="modal"> Reset password
+                    data-dismiss="modal"> Reset password 
+
                 </button>
             </div>
             <!-- <div>
