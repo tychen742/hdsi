132de88685a364e75c71b8f08de5f809b7d65689
Tsangyao Chen <tychen742@gmail.com>
2024-12-30 19:57:16 -0600

    committed @ 2024-1230-195716

--- includes/headers.php
+++ includes/headers.php
@@ -77,3 +77,47 @@ if ($server_name == 'hdsinet.us') {
 
 <dody>
 
+<?php include_once 'topnav.php'; ?>
+
+    <div class="container" style="width: 90%; max-width: 900px">
+
+        <?php
+        /// http://talkerscode.com/webtricks/password-reset-system-using-php.php
+        $email_email = $_GET['key'];
+        $token_email = $_GET['reset'];
+        if (isset($email_email) && isset($token_email)) {
+            try {
+                $stmt = $pdo->prepare(" SELECT email, account_verify_token FROM user WHERE email = '$email_email' ");
+                $stmt->execute();
+                $result = $stmt->fetch();
+                $email_db = $result['email'];
+                $token_db = $result['account_verify_token'];
+
+            } catch (Exception $exception) {
+                echo $exception;
+                echo $exception;
+            }
+
+            if (!isset($email_db)) {
+                echo "<div id='php-message'> Having problem with this email address. <br>
+ Redirecting... </div>";
+                echo "<meta http-equiv=REFRESH CONTENT=3;url=$p/index.php>";
+            } elseif ($token_email != $token_db) {
+                echo "<div id='php-message'> Hmmm... the reset token is not valid. <br>
+ Redirecting to HDSI home... </div>";
+                echo "<meta http-equiv=REFRESH CONTENT=3;url=$p/index.php>";
+            } else {
+
+                ?>
+                <form method="POST" id="php-message">
+                    <input type="hidden" name="email_email"
+                           value="<?php echo $email_email; ?>">
+                    <div> Enter new password:</div>
+                    <input type="password" name='password'>
+                    <input type="submit" name="submit_password" value="Reset">
+                </form>
+                <?php
+            }
+        }
+        ?>
+    </div>
